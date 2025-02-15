095538f13856816e12c830163f0abef66af74c86
jwlebert <j.lebert4402@gmail.com>
2025-02-15 17:39:31 -0500

    added firebase

--- next/src/app/whos-next/[partner]/page.tsx
+++ next/src/app/whos-next/[partner]/page.tsx
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAcp9mLAkaVOm77GjeEciuIFe8tOjf_nEM",
+  authDomain: "twocents-19b16.firebaseapp.com",
+  projectId: "twocents-19b16",
+  storageBucket: "twocents-19b16.firebasestorage.app",
+  messagingSenderId: "606274909309",
+  appId: "1:606274909309:web:3ff84b92f8682f772aad14",
+  measurementId: "G-X7D2D74010"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function Page() {
     return (
       <div className="grid grid-cols-10">
