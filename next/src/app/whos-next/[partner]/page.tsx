940448937002ab3cdeeb64942e88ed50d2a260a9
jwlebert <j.lebert4402@gmail.com>
2025-02-15 17:26:54 -0500

    reformated to be different app

---
+++ next/src/app/whos-next/[partner]/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+      <div className="grid grid-cols-10">
+            <div id="user" className="col-span-3 h-screen bg-green-800">
+              
+            </div>
+            <div id="expenses" className="col-start-4 col-span-7 h-screen bg-red-100">
+              
+            </div>
+        </div>
+    );
+  }
