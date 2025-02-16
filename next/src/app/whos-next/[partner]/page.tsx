ba20f6d84c5049716c9466fe6a402b7615195d5d
jwlebert <j.lebert4402@gmail.com>
2025-02-15 23:36:21 -0500

    added basic login, signup, logout

--- next/src/app/whos-next/[partner]/page.tsx
+++ next/src/app/whos-next/[partner]/page.tsx
@@ -1,30 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+"use client";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAcp9mLAkaVOm77GjeEciuIFe8tOjf_nEM",
-  authDomain: "twocents-19b16.firebaseapp.com",
-  projectId: "twocents-19b16",
-  storageBucket: "twocents-19b16.firebasestorage.app",
-  messagingSenderId: "606274909309",
-  appId: "1:606274909309:web:3ff84b92f8682f772aad14",
-  measurementId: "G-X7D2D74010"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+import { useEffect, useState } from "react";
+import { auth } from "src/lib/firebase";
+import { User } from "firebase/auth";
 
 export default function Page() {
+    const [user, setUser] = useState<User | null>(null)
+
+    // Listen for authentication state changes
+    useEffect(() => {
+      const unsubscribe = auth.onAuthStateChanged(setUser)
+      return () => unsubscribe() // Cleanup on component unmount
+    }, [])
+
     return (
       <div className="grid grid-cols-10">
             <div id="user" className="col-span-3 h-screen bg-green-800">
-              
+              {user?.email ?? 'no user'}
             </div>
             <div id="expenses" className="col-start-4 col-span-7 h-screen bg-red-100">
               
