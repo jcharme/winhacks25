86a83ac3f1ebb48c075e924464d6ed1d35e223a7
jwlebert <j.lebert4402@gmail.com>
2025-05-23 15:57:16 -0400

    query for groups containing given usre

--- next/src/app/whos-next/page.tsx
+++ next/src/app/whos-next/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react"
+import { use, useEffect, useState } from "react"
 import Link from "next/link"
-import { auth } from "@lib/firebase"
+import { auth, db } from "@lib/firebase"
 import { User } from "firebase/auth";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { setgroups } from "process";
+
+interface Group {
+    id: string;
+    name?: string;
+    users?: string[];
+}
 
 export default function Page() {
     const [user, setUser] = useState<User | null>(null);
@@ -11,6 +19,26 @@ export default function Page() {
         const unsub = auth.onAuthStateChanged(setUser);
         return () => unsub();
     }, []); // user may be null initially then update after page load
+
+    const [groups, setGroups] = useState<Group[] | null>([]);
+    useEffect(() => {
+        if (user === null) {
+            setGroups([]);
+            return;
+        }
+
+        async function fetchGroups() {
+            const groupsRef = collection(db, "whos-next");
+            const q = query(groupsRef, where("users", "array-contains", user?.uid));
+            const snapshot = await getDocs(q);
+            const data = snapshot.docs.map(doc => ({
+                id: doc.id, ...doc.data()
+            }));
+            setGroups(data);
+        }
+
+        fetchGroups();
+    }, [user?.uid]);
 
     return (
         <>
@@ -20,7 +48,14 @@ export default function Page() {
             :
             <div>
                 <div>creating groups goes here</div>
-                <div>group list here</div>
+                <div>
+                    {
+                        groups?.length !== 0 ?
+                        <ul>
+                            
+                        </ul> : <p>No groups loaded.</p>
+                    }
+                </div>
             </div>
         }
         </>
