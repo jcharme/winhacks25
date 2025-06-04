f01333f7410f78066f0f6f69de6f4638f404927b
jwlebert <j.lebert4402@gmail.com>
2025-06-04 14:45:45 -0400

    started to isolate login/signup into components

--- next/src/app/login/page.tsx
+++ next/src/app/login/page.tsx
@@ -1,123 +1,190 @@
 "use client";
 import { ChangeEvent, FormEvent, useState } from "react";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, User } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  User,
+} from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
-import { auth, db } from "@lib/firebase"
+import { auth, db } from "@lib/firebase";
 
-export default function Login() {
-    const [user, setUser] = useState<User | null>(null);
-    const [action, setAction] = useState<"login" | "signup">('signup');
-    const [credentials, setCredentials] = useState({
-        email: '',
-        password: '',
-    })
+interface LoginData {
+  email: string;
+  password: string;
+}
 
-    const [username, setUsername] = useState("");
-    const [name, setName] = useState("");
+const Login = (
+    setUser: {setUser: React.Dispatch<React.SetStateAction<User | null>>;}
+) => {
+  const [data, setData] = useState<LoginData>();
 
-    const handleCredentialsChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setCredentials((prev) => ({
-            ...prev, [name]: value,
-        }));
-    }
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    if (!data?.email || !data?.password) return null;
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+      })
+      .catch((error) => {
+        const errCode = error.code;
+        const errMsg = error.message;
+        console.log(errCode, errMsg);
+      });
+  };
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (action == 'signup') {
-            createUserWithEmailAndPassword(auth, credentials.email, credentials.password)
-                .then((userCredential) => {
-                    const user = userCredential.user;
-                    setUser(user);
+  const handleCredentialsChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCredentials((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-                    return setDoc(doc(db, "users", user.uid), {
-                        email: user.email, name, username                 
-                    });
-                })
-                .catch((error) => {
-                    const errCode = error.code;
-                    const errMsg = error.message;
-                    console.log(errCode, errMsg);
-                });
-        } else if (action == 'login') {
-            signInWithEmailAndPassword(auth, credentials.email, credentials.password)
-                .then((userCredential) => {
-                    setUser(userCredential.user);
-                })
-                .catch((error) => {
-                    const errCode = error.code;
-                    const errMsg = error.message;
-                    console.log(errCode, errMsg);
-                });
-        }
-    }
+  return (
+    <form onSubmit={handleLogin} className="m-4 space-y-2">
+      <label className="block">
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={credentials.email}
+          onChange={handleCredentialsChange}
+        />
+      </label>
+      <label className="block">
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleCredentialsChange}
+        />
+      </label>
+      <button type="submit">Click to {action}</button>
+    </form>
+  );
+};
 
-    const handleSignOut = (e: FormEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        signOut(auth)
-            .then(() => {
-                setUser(null);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
+interface SignupData extends LoginData {
+  name: {
+    first: string;
+    last: string;
+  };
+  username: string;
+}
 
-    return (
-        <div>
-            {
-                user == null ?
-                <form onSubmit={handleSubmit} className="m-4 space-y-2">
-                    <select className="block" id="dropdown" value={action} onChange={(e) => {setAction(e.target.value as "login" | "signup")}}>
-                        <option value="login">Login</option>
-                        <option value="signup">Sign up</option>
-                    </select>
-                    {
-                        action == "signup" ? <>
-                            <label className="block">
-                                Username:
-                                <input
-                                    type="text"
-                                    name="username"
-                                    value={username}
-                                    onChange={(change) => {setUsername(change.target.value)}}
-                                />
-                            </label>
-                            <label className="block">
-                                Name:
-                                <input
-                                    type="text"
-                                    name="name"
-                                    value={name}
-                                    onChange={(change) => {setName(change.target.value)}}
-                                />
-                            </label>
-                        </> : null
-                    }
-                    <label className="block">
-                        Email:
-                        <input
-                            type="email"
-                            name="email"
-                            value={credentials.email}
-                            onChange={handleCredentialsChange}
-                        />
-                    </label>
-                    <label className="block">
-                        Password:
-                        <input
-                            type="password"
-                            name="password"
-                            value={credentials.password}
-                            onChange={handleCredentialsChange}
-                        />
-                    </label>
-                    <button type="submit">Click to {action}</button>
-                </form> :
-                <button onClick={handleSignOut}>Log out</button>
+const Signup = () => {
+  const [data, setData] = useState<SignupData>();
 
-            }
-            
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      credentials.email,
+      credentials.password
+    )
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+
+        return setDoc(doc(db, "users", user.uid), {
+          email: user.email,
+          name,
+          username,
+        });
+      })
+      .catch((error) => {
+        const errCode = error.code;
+        const errMsg = error.message;
+        console.log(errCode, errMsg);
+      });
+  };
+
+  return (
+    <form>
+      {/* <label className="block">
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={(change) => {
+                setUsername(change.target.value);
+              }}
+            />
+          </label>
+          <label className="block">
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(change) => {
+                setName(change.target.value);
+              }}
+            />
+          </label>
+      <label className="block">
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={credentials.email}
+          onChange={handleCredentialsChange}
+        />
+      </label>
+      <label className="block">
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleCredentialsChange}
+        />
+      </label>
+      <button type="submit">Click to {action}</button> */}
+    </form>
+  );
+};
+
+export default function Account() {
+  const [user, setUser] = useState<User | null>(null);
+  //   const [action, setAction] = useState<"login" | "signup">("signup");
+  //   const [credentials, setCredentials] = useState({
+  //     email: "",
+  //     password: "",
+  //   });
+
+  //   const [username, setUsername] = useState("");
+  //   const [name, setName] = useState("");
+
+  const handleSignOut = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      {user ? (
+        <div className="flex">
+          <div className="w-[50vw] h-screen">
+            <Login setUser={setUser} />
+          </div>
+          <div className="text-background bg-secondary w-[50vw] h-screen">
+            <Signup />
+          </div>
         </div>
-    );
-  }
+      ) : (
+        <p>logout - todo</p>
+      )}
+    </>
+  );
+}
